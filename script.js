bd0022b112f38ee7a784e071fdd00af0e61f4cea
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-11-01 12:14:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let lin = document.getElementById("line");
+let angle=0;
+function rotateLin(){
+	angle += 2;
+	lin.style.transform = `translateX(-50%) rotate(${angle}deg)`;
+	requestAnimationFrame(rotateLin);
+}
+requestAnimationFrame(rotateLin);
+// rotateLin();
