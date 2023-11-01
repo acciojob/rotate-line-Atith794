96faf2f2da1fde171cc103e03591abc794a29048
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-11-01 12:38:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 	let angle=0;
 	function rotateLin(){
 		angle += 2;
-		lin.style.transform = `translateX(-50%) rotate(${angle}deg)`;
+		lin.style.transform = `translateX(-50%) translateY(-50%) rotate(${angle}deg)`;
 	}
 
 	setInterval(rotateLin,20);
