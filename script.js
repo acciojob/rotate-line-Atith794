a0ca140f70cb99a60f8c9d300d2062c685bf6c4a
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-11-01 12:34:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,19 +10,19 @@
 	setInterval(rotateLin,20);
 
 
-it('rotates the line every 2 milliseconds', () => {
-  cy.visit('your_page_url_here');
+// it('rotates the line every 2 milliseconds', () => {
+//   cy.visit('your_page_url_here');
 
-  // Wait for the #line element to be available in the DOM
-  cy.get('#line').should('exist');
+//   // Wait for the #line element to be available in the DOM
+//   cy.get('#line').should('exist');
 
-  cy.get('#line').then(($element) => {
-    const initialTransform = $element.css('transform');
-    cy.wait(2000).then(() => { // Wait for 2000ms (2 seconds) to match your requirement
-      const updatedTransform = $element.css('transform');
-      expect(updatedTransform).not.to.equal(initialTransform);
-    });
-  });
-});
+//   cy.get('#line').then(($element) => {
+//     const initialTransform = $element.css('transform');
+//     cy.wait(2000).then(() => { // Wait for 2000ms (2 seconds) to match your requirement
+//       const updatedTransform = $element.css('transform');
+//       expect(updatedTransform).not.to.equal(initialTransform);
+//     });
+//   });
+// });
 // requestAnimationFrame(rotateLin);
 // rotateLin();
